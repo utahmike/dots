b2ae15bbb109ee948e029e32dea9847d858eb364
Michael Clark <utahmike@users.noreply.github.com>
2024-10-28 21:03:22 -0400

    New Comment Plugin

--- config/nvim/lua/mjc/lazy/plugins/init.lua
+++ config/nvim/lua/mjc/lazy/plugins/init.lua
@@ -1,5 +1,7 @@
 return {
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 	"tpope/vim-fugitive",
+	"tomtom/tcomment_vim",
+
 	-- "christoomey/vim-tmux-navigator", -- tmux & split window navigation
 }
