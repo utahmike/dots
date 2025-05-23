d6989878bd9cf4c4d931e80718fe03674a8d4002
Michael Clark <michael.clark@beyondidentity.com>
2025-05-23 09:53:19 -0400

    Move to LazyVim

--- config/nvim/lua/mjc/lazy/plugins/init.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"nvim-lua/plenary.nvim",
-	"tpope/vim-fugitive",
-	"tomtom/tcomment_vim",
-	"blueyed/vim-diminactive",
-	-- "christoomey/vim-tmux-navigator", -- tmux & split window navigation
-}
